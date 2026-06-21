cb8e1ef040215513beabb301af0df27256c2ba9a
mohitsoni705 <mohitravet07@gmail.com>
2026-06-21 19:35:08 +0530

    fix download button

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Resume = () => {
   return (
     <div>
-  <a href="./public/resume/Mohit__2025.pdf" download="Mohit_Resume_2025.pdf">
+      
+  <a href="https://raw.githubusercontent.com/mohitsoni705/MyWeb/main/public/resume/Mohit__2025.pdf" target="_blank"
+  rel="noopener noreferrer">
     <button className='px-8 py-4 bg-gradient-to-r from-gray-700 to-black text-white rounded-lg hover:from-gray-600 hover:to-gray-900 transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl border border-gray-600'>
       Resume
     </button>
